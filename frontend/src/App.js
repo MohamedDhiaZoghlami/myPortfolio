97f490bf8a565fb905e5486fdc9c27c170355f39
MohamedDhiaZoghlami <mohameddhia.zoghlami@esprit.tn>
2022-07-27 13:45:55 +0100

    header section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./App.scss";
+import { About, Work, Header, Footer, Skills } from "./container";
+import { Navbar } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
