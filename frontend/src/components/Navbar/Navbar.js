2fe94c744b19f400b409a507049643b3a08357fc
MohamedDhiaZoghlami <mohameddhia.zoghlami@esprit.tn>
2022-08-13 18:44:47 +0100

    starting backend

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -9,7 +9,8 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        {/* <img src={images.logo} alt="logo" /> */}
+        <h1 style={{ color: "#6b7688" }}>Dzovi</h1>
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
--- frontend/src/container/Header/Header.js
+++ frontend/src/container/Header/Header.js
@@ -55,13 +55,11 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.react, images.javascript, images.redux].map(
-          (circle, index) => (
-            <div className="circle-cmp app__flex" key={`circle-${index}`}>
-              <img src={circle} alt="skills" />
-            </div>
-          )
-        )}
+        {[images.react, images.javascript, images.node].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="skills" />
+          </div>
+        ))}
       </motion.div>
     </div>
   );
